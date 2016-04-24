71317fa4817888bce3a480a68af4193803d5e8eb
Porawit Poboonma <porawit.p@bizidea.co.th>
2016-04-25 01:27:40 +0700

    add docker-compose and neccessary files

---
+++ docker/entrypoint/sentry.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+function start_sentry {
+    echo "Waiting for postgresql service"
+
+    # wait for mysql service
+    # while [ "$(nc -v -z -w 3 $SENTRY_POSTGRES_HOST 5432 2> /dev/null; echo $?)" == "1" ];
+    while [ "$(sentry config get system.admin-email 2>&1 | grep "Connection refused" > /dev/null; echo $?)" == "0" ];
+    do
+        echo "Retry in 2 seconds."
+        sleep 2
+    done
+
+    # test if sentry already installed
+    # sentry config get system.admin-email
+    # not installed
+    # "$(sentry config get system.admin-email 2>&1 | grep "Connection refused" > /dev/null; echo $?)" == "0"
+    if [ "$(sentry config get system.admin-email 2>&1 | grep "Unable to fetch internal project" > /dev/null; echo $?)" == "0" ]; then
+        sentry upgrade --noinput
+        sentry createuser --email "$SENTRY_EMAIL" --password "$SENTRY_PASSWORD" --superuser --no-input
+    fi
+
+    sentry celery beat&
+    sentry celery worker&
+    sentry start
+}
+
+# first check if we're passing flags, if so
+# prepend with sentry
+if [ "${1:0:1}" = '-' ]; then
+    set -- sentry "$@"
+fi
+
+case "$1" in
+    start)
+        start_sentry
+    ;;
+    celery|cleanup|config|createuser|devserver|django|export|help|import|init|plugins|queues|repair|shell|upgrade)
+        set -- sentry "$@"
+    ;;
+    generate-secret-key)
+        exec sentry config generate-secret-key
+    ;;
+esac
+
+exec "$@"
+
